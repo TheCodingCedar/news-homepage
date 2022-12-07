6a65974c223450772145a2d797e79fc4717d2f61
Willie Santos <willie_santos@hotmail.com>
2022-12-07 18:29:14 -0300

    update project

---
+++ menu.js
@@ -0,0 +1,14 @@
+/* var bur = document.getElementById('burguer')
+var itens = document.getElementById('itens')
+
+function clickMenu() {
+    if(itens.style.display = 'none') {
+        itens.style.display = 'flex'
+        bur.innerHTML = `<span id="burguer" class="material-icons" onclick="clickMenu()">close</span>`               
+    } else {
+        itens.style.display = 'flex'        
+        bur.innerHTML = `<span id="burguer" class="material-icons" onclick="clickMenu()">menu</span>`        
+    }
+
+}
+ */
